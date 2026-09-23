e68d0dc5f80736d25b4851e44e3da3216c4cac55
Wilson <victoronuoha03@gmail.com>
2026-09-23 05:08:31 -0700

    removed the exmple codes

--- data/products.js
+++ data/products.js
@@ -36,26 +36,6 @@ class Products {
   }
 }
 
-/*const product1 = new Products(
-  {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-    keywords: [
-      "socks",
-      "sports",
-      "apparel"
-    ]
-  }
-);
-console.log(product1);*/
-
-
 
 export const products = [
   {
