c1af03d8f3c162e207ecea3055bf6abcdd2e7331
Wilson <victoronuoha03@gmail.com>
2026-09-24 09:16:07 -0700

    loadproducts in before all hook in jasmine to test if our back-end is passing

--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -1,9 +1,14 @@
 import { renderCheckoutPage } from "../../scripts/checkout/orderSummary.js";
 import { loadFromStorage } from "../../data/cart.js";
+import { loadProducts } from "../../data/products.js";
 
 
 const prodID = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
 const prodID2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
+beforeAll((done) =>{
+  loadProducts(()=>{done()});
+})
 
 describe('Test Suite: renderCheckoutPage', () =>{
   it ('Displays the cart', () => {
